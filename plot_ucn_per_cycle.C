bda5ed2976886af1f08bc113999f52a592e0de9d
Thomas Lindner <lindner@triumf.ca>
2017-11-06 16:03:38 -0800

    First draft of example macro of UCN count vs cycle

---
+++ plot_ucn_per_cycle.C
@@ -0,0 +1,96 @@
+// Macro for finding the number of UCN per cycle.
+
+
+
+plot_ucn_per_cycle(std::string infile){
+  
+  
+  
+  ULong64_t eventTot;
+  double cycleStart;
+
+  TFile* fin = new TFile(infile.c_str(),"READ");
+  if (fin == NULL) {
+    std::cout<<"could not open file : "<<infile<<std::endl;
+    return;
+  } 
+  if ( fin->IsOpen() == false ) {
+    std::cout<<"file not open : "<<infile<<std::endl;
+    
+    return;
+  }
+
+  fin->ls();
+  
+  
+  TTree* uin = (TTree*) fin->Get("UCNHits_Li-6");
+  TTree* truntime = (TTree*) fin->Get("RunTransitions_Li-6");
+
+  uin->Print();
+  
+  truntime->Print();
+  std::vector<double> cycleStartTimes;
+  std::vector<int> numberEventsPerCycle;
+
+  // Loop over the transitions, save the start of each transition and make a counter.
+  truntime->SetBranchAddress("tUnixTimeTransition",&cycleStart);
+  eventTot = (Double_t)truntime->GetEntries();
+  for(ULong64_t j=0;j<eventTot;j++) {
+    truntime->GetEvent(j);
+    cycleStartTimes.push_back(cycleStart);
+    numberEventsPerCycle.push_back(0);
+
+  }
+  
+  //  Now loop over the UCN hits.  Calculate the right number of hits for each cycle.
+  double tUnixTimePrecise;
+  Float_t tPSD;  
+  UShort_t tChargeL;
+  UShort_t tIsUCN, tChannel;
+  uin->SetBranchAddress("tUnixTimePrecise",&tUnixTimePrecise);
+  uin->SetBranchAddress("tIsUCN",&tIsUCN);
+  uin->SetBranchAddress("tChannel",&tChannel);
+  uin->SetBranchAddress("tPSD",&tPSD);
+  uin->SetBranchAddress("tChargeL",&tChargeL);
+
+  // Make on plot showing PSD vs QL for channel 0.
+  TH2F *psd_vs_ql = new TH2F("psd_vs_ql","PSD vs QL",200,0,15000,200,-1,1);
+
+  eventTot = (Double_t)uin->GetEntries();
+  for(ULong64_t j=0;j<eventTot;j++) {
+    uin->GetEvent(j);
+    // Check if this is a UCN hit.
+    if(tIsUCN){
+      // check which cycle this is in...
+      for(unsigned int i = 0; i < cycleStartTimes.size()-1; i++){
+	//std::cout << tUnixTimePrecise << " " <<  cycleStartTimes[i] << " " << cycleStartTimes[i+1] << std::endl;
+	if(tUnixTimePrecise > cycleStartTimes[i] && tUnixTimePrecise <= cycleStartTimes[i+1]){
+	  numberEventsPerCycle[i]++;
+	  break;
+	}
+      }
+      
+    }
+
+    if(tChannel == 0){
+      psd_vs_ql->Fill(tChargeL,tPSD);
+    }
+    
+  }
+
+
+  // Plot PSD vs QL
+  TCanvas *c1 = new TCanvas(,"c1","PSD vs QL");
+  psd_vs_ql->Draw("COL");
+
+  // Plot the UCN per cycle
+  TGraph *ucn_per_cycle = new TGraph();
+  for(unsigned int i = 0; i < cycleStartTimes.size()-1; i++){
+    ucn_per_cycle->SetPoint(i,cycleStartTimes[i],numberEventsPerCycle[i]);
+  }
+  TCanvas *c2 = new TCanvas("c2","UCN vs cycle");
+
+  ucn_per_cycle->Draw("AP*");
+
+  
+}
